e372134d0502720564e296276cb8dcceb98241b5
Kaustubh Agarwal <kaustubhagarwal18@gmail.com>
2020-02-21 14:11:27 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,4 +13,7 @@ ls
 # Monitor 2.4 and 5 GHz channels with
 #time interval 0.2 sec and write the data to file
 sudo airodump-ng -M -f 200 --band abg  --write asdf wlp2s0mon   
-
+# start networking again
+sudo airmon-ng stop wlp2s0mon
+sudo service networking start
+sudo service network-manager start
